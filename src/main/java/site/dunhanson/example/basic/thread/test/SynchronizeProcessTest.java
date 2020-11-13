dcb0cfae819adcf7e530c4e713f7dcd4bd6837d2
dunhanson <dunhanson@gmail.com>
2020-11-14 05:49:50 +0800

    注释完善

--- src/main/java/site/dunhanson/example/basic/thread/test/SynchronizeProcessTest.java
+++ src/main/java/site/dunhanson/example/basic/thread/test/SynchronizeProcessTest.java
@@ -7,7 +7,15 @@ import site.dunhanson.example.basic.thread.process.UnThreadSafeCountingProcessor
 public class SynchronizeProcessTest {
     public static final int LOOP_TIME = 100 * 100 * 1000;
 
-
+    /**
+     * ynchronize
+     * 如何向一个新手介绍synchronized的表象？
+     * Java语言的关键字，当它用来修饰一个方法或者一个代码块的时候，能够保证在同一时刻最多只有一个线程执行该段代码。
+     *
+     * 如何在一个老司机面前装逼格？
+     * Java中的synchronize，通过使用内置锁
+     * 来实现对变量的同步操作，进而实现了对变量操作的原子性和其他线程对变量的可见性，从而确保了并发情况下的线程安全。
+     */
     public static void main(String[] args) {
         CountingProcessor countingProcessor = new UnThreadSafeCountingProcessor();
         runTask(countingProcessor);
--- src/main/java/site/dunhanson/example/basic/thread/test/VolatileGameTest.java
+++ src/main/java/site/dunhanson/example/basic/thread/test/VolatileGameTest.java
@@ -4,6 +4,11 @@ import site.dunhanson.example.basic.thread.game.GoalNotifier;
 
 public class VolatileGameTest {
 
+    /**
+     * 答案已经很明确了，这里面有两个线程，main函数所在的是主线程和GoalNotifier线程，这两个线程都分别从主内存从拷贝了一个goal变量的副本
+     * 所以当main函数调用setGoal()方法修改goal时，修改的其实是自己线程工作空间上的那个副本goal
+     * 对主内存的goal没有影响，对GoalNotifier线程的goal副本更加没有影响，GoalNotifier线程自然就感知不到goal变成true了。
+     */
     public static void main(String[] args) throws InterruptedException {
         // Game begun! Init goalNotifier thread
         GoalNotifier goalNotifier = new GoalNotifier();
